98af0c53ea57a4e0c375ac38854dfcc0d30bf776
Watana.mut <6812100025@mut.ac.th>
2025-11-23 17:18:20 +0700

    Lab 2

---
+++ LAB01/TEST02/Lab 2/Lab 2.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	int age = 21;
+	float height = 171.5;
+	float weight = 52.7;
+	string name = "Watana Phasatid";
+	string Number = "0930924194";
+
+	cout << "Watana Phasatid" << endl;
+	cout << "--------------" << endl;
+	cout << "Name " << name << endl;
+	cout << "Student ID 6812100025" << endl;
+	cout << "Mos " << age << endl;
+	cout << "Height " << height << "cm" << endl;
+	cout << "Weight " << weight << "kg" << endl;
+	cout << "Email Watana.mos@gmail.com" << endl;
+	cout << "Number " << Number << endl;
+	return 0;
+}
+
